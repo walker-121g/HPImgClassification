93261ac78ee825b1c96969bd1a6bef146f86f250
Edward <wcamp@121g.io>
2023-10-04 15:45:08 -0400

    switch back to original model

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ using Tensorflow;
 
 string folder = Path.Combine(Environment.CurrentDirectory, "./Assets");
 string imageFolder = Path.Combine(folder, "Training");
-string modelFile = Path.Combine(folder, "saved_model.pb");
+string modelFile = Path.Combine(folder, "model.pb");
 string trainingFile = Path.Combine(folder, "training.tsv");
 string testingFile = Path.Combine(folder, "test.tsv");
 
