f2f5ef197c972fd37a71875962e829cc0f3ed4cd
Edward <wcamp@121g.io>
2023-10-09 09:59:14 -0400

    switch out image 9

--- Program.cs
+++ Program.cs
@@ -23,7 +23,7 @@ void ClassifyImage(MLContext mlContext, ITransformer model)
 {
     ImageData image = new ImageData()
     {
-        ImagePath = Path.Combine(imageFolder, "2021-11-15_5_58_22-HuntPro.png")
+        ImagePath = Path.Combine(imageFolder, "2021-10-21_5_22_36-HuntPro.png")
     };
 
     PredictionEngine<ImageData, ImagePrediction> engine = mlContext.Model.CreatePredictionEngine<ImageData, ImagePrediction>(model);
